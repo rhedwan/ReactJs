226633cfb774e5e0f92d5cb9e66c18a4486c6f80
rhedwan <adesolaridwan2003@gmail.com>
2021-12-25 21:28:51 +0100

    Render more Nested components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -19,14 +19,16 @@ NOTE: JSX Rules
 function Greeting() {
   return (
       <div >
-        <Person/>
-        <p>This is my message</p>
+        <Person />
+        <Message />
       </div>
   );
 };
 
 const Person = () => <h2>John Doe</h2>;
-
+const Message = () => {
+  return  <p>This is my message</p>;
+}
 
 // const Greeting = () => {
 //   return  React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'));
