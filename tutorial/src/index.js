aa90651b51361a9d14422d13fd8f58b06b697926
rhedwan <adesolaridwan2003@gmail.com>
2021-12-25 22:00:31 +0100

    Mini Book Project

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -9,19 +9,29 @@ function BookList() {
       <Book />
       <Book />
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 };
 
 const Book = () => {
-  return <article>
-  This is a book
-  </article>
-}
+  return (
+    <article>
+      <Image> </Image>
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <img
+    src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt=""
+  />
+}; 
+
+const Title = () => <h1>I Love You to the Moon and Back </h1>;
+const Author = () => <h4> Amelia Hepworth</h4>;
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
