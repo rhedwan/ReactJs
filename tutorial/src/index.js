f3a717d83a0caf93f017eadeb72747bfe5554849
rhedwan <adesolaridwan2003@gmail.com>
2021-12-25 21:35:13 +0100

    Cleaning of the code for BookList

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,37 +1,17 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-// Stateless functional component
-// Always returns a JSX
-
-/* 
-NOTE: JSX Rules
-1. return single element
-2. div / section / article or fragment
-3. use camelCase for html attributes
-4. className instead of class
-5. close every element
-6. formatting
- */
-
 // Nested components and React Tools
 
-function Greeting() {
+function BookList() {
   return (
-      <div >
-        <Person />
-        <Message />
-      </div>
+      <section >
+       This is a booklist
+      </section>
   );
 };
 
-const Person = () => <h2>John Doe</h2>;
-const Message = () => {
-  return  <p>This is my message</p>;
-}
 
-// const Greeting = () => {
-//   return  React.createElement('div', {}, React.createElement('h1', {}, 'Hello World'));
-// };
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+
+ReactDom.render(<BookList />, document.getElementById('root'));
