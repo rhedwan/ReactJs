8f0b4b6c100a2546e2436353f884bbacb1fc59e3
rhedwan <adesolaridwan2003@gmail.com>
2021-12-25 20:12:24 +0100

    Render the first component using arrow function & createElement

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function Greeting() {
-  return  <h4>this is Ridwan and this my very first component </h4>;
+// Stateless functional component
+// Always returns a JSX
+// function Greeting() {
+//   return  <h4>this is Ridwan and this my very first component!!</h4>;
+// };
+
+const Greeting = () => {
+  return  React.createElement('h4', null, 'Hello World. this is Ridwan and this my very first component!!');
 };
 
 ReactDom.render(<Greeting />, document.getElementById('root'));
