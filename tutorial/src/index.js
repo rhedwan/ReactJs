c9af2d99ba125f74c6bf211aa1bce4c6022dfe6e
rhedwan <adesolaridwan2003@gmail.com>
2021-12-25 19:55:10 +0100

    Render the first component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 function Greeting() {
   return  <h4>this is Ridwan and this my very first component </h4>;
 };
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
