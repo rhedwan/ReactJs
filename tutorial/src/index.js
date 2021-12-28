fa6f7173ec4c0c6897580372a75d39fe3873d130
rhedwan <adesolaridwan2003@gmail.com>
2021-12-28 19:46:13 +0100

    Cleanup of the existing BookList component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -10,41 +10,30 @@ function BookList() {
   return (
     <section className="bookList">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 };
 
 const Book = () => {
   return (
-    <article className ='book'>
-      <Image> </Image>
-      <Title />
-      <Author />
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+        alt=""
+      />
+      <h1>I Love You to the Moon and Back </h1>
+      <h4 style=
+      {
+        {
+          color: '#617d98',
+          fontSize : '0.75rem',
+          marginTop: '0.25rem'
+        }
+      }> Amelia Hepworth
+      </h4>;
     </article>
   );
 };
-
-const Image = () => {
-  return <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  />
-}; 
-
-const Title = () => <h1>I Love You to the Moon and Back </h1>;
-const Author = () => <h4 style={
-  {
-    color: '#617d98',
-    fontSize : '0.75rem',
-    marginTop: '0.25rem'
-  }
-}> Amelia Hepworth</h4>;
 
 
 ReactDom.render(<BookList />, document.getElementById('root'));
