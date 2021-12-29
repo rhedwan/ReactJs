872efb6f15c2f0b6890168562b902ecce24b3cef
rhedwan <adesolaridwan2003@gmail.com>
2021-12-29 09:46:23 +0100

    Event Basics: Using Refrence Example / passing of function

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -37,12 +37,21 @@ function BookList() {
 }
 
 const Book = ({ img, title, author }) => {
-  // const { img, title, author } = props;
+  // Attributes, eventHandler
+  // onClick, onMouseOver
+
+  const clickHandler = () => {
+    alert('Hello world')
+  };
+  
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler}>
+      Refrence Example
+      </button>
     </article>
   );
 };
