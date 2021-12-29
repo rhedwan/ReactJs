1adbbe707066ff10aa50c72651fab3785dcff373
rhedwan <adesolaridwan2003@gmail.com>
2021-12-29 12:00:50 +0100

    Import and Export: Transfer the books to a module

---
+++ tutorial/src/books.js
@@ -0,0 +1,20 @@
+export const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "I Love You to the Moon and Back !!!",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a family",
+    author: "Shannon Olsen",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/41EzNnr4YUL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "The Vanishing Half: A Novel",
+    author: "Brit Bennett",
+  }
+];
