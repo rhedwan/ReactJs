9c5148eabf22a6f656aafee7c7e977de84ffc544
rhedwan <adesolaridwan2003@gmail.com>
2021-12-29 09:26:50 +0100

    Fixed Warning Each child in a list should have a unique 'key' prop

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -7,16 +7,19 @@ import "./index.css";
 // Setup variables
 const books = [
   {
+    id: 1,
     img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "I Love You to the Moon and Back !!!",
     author: "Amelia Hepworth",
   },
   {
+    id: 2,
     img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
     title: "Our Class is a family",
     author: "Shannon Olsen",
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/41EzNnr4YUL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "The Vanishing Half: A Novel",
     author: "Brit Bennett",
@@ -27,7 +30,7 @@ function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book key={book.id} book={book}></Book>;
       })}
     </section>
   );
