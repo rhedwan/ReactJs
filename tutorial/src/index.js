a87ce9cd43588e8eb7fb4c9486b91c8619fbd444
rhedwan <adesolaridwan2003@gmail.com>
2021-12-29 10:06:22 +0100

    Event Basics: Using of 'onMouseOver'

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -51,7 +51,12 @@ const Book = ({ img, title, author }) => {
   };
 
   return (
-    <article className="book">
+    <article
+      className="book"
+      onMouseOver={() => {
+        console.log(title);
+      }}
+    >
       <img src={img} alt="" />
       <h1 onClick={() => console.log(title)}>{title}</h1>
       <h4>{author}</h4>
@@ -66,7 +71,6 @@ const Book = ({ img, title, author }) => {
 };
 
 ReactDom.render(<BookList />, document.getElementById("root"));
-
 
 /* 
 Event in React
