2c6c8203e369de5f4e2a3d79637ef720263c2b74
rhedwan <adesolaridwan2003@gmail.com>
2021-12-31 21:53:27 +0100

    Rendered the component

--- react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,13 +3,18 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
-  return <React.Fragment>
-  {
-    people.map( (person) => {
-      return 'Hello'
-    })
-  }
-  </React.Fragment>;
+  return (  
+    <React.Fragment>
+      {people.map((person) => {
+        const {id, name} = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default UseStateArray;
