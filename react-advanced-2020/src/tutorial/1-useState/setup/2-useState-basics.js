b2c54a738b86bb06b875ed606605df50076f42c4
rhedwan <adesolaridwan2003@gmail.com>
2021-12-31 19:48:33 +0100

    Destructure the useState return

--- react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  console.log(useState('Hello world'));
-  const value = useState(1)[0];
-  const handler = useState(1)[1];
-  console.log(value, handler) ;
-  return <h2>useState basic example</h2>;
+  // console.log(useState('Hello world'));
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler) ;
+  const [text, setText] = useState("randon title");
+
+  return <React.Fragment>
+   <h1>{text}</h1>
+  </React.Fragment>
 };
 
 export default UseStateBasics;
