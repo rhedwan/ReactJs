f87eacd8a70d3d7284ae60e49b429cb07e538e0c
rhedwan <adesolaridwan2003@gmail.com>
2021-12-31 22:00:59 +0100

    Made a clear button with useState

--- react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,16 +3,19 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
-  return (  
+  return (
     <React.Fragment>
       {people.map((person) => {
-        const {id, name} = person;
+        const { id, name } = person;
         return (
           <div key={id} className="item">
             <h4>{name}</h4>
           </div>
         );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear items
+      </button>
     </React.Fragment>
   );
 };
