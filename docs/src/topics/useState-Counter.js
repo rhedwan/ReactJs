7616479aa45591674fcb63ef4af53ad292f7e200
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 10:01:46 +0100

    Made quick UIfor the counter

--- docs/src/topics/useState-Counter.js
+++ docs/src/topics/useState-Counter.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export function Counter(){
-    return (
-        <React.Fragment>
-            <h1>Building a Counter with useState</h1>
-        </React.Fragment>
-    )
+export function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <React.Fragment>
+      <h1>Building a Counter with useState</h1>
+      <div>
+        <h3>You have clicked this {count} times</h3>
+        <button>Click Me</button>
+      </div>
+    </React.Fragment>
+  );
 }
