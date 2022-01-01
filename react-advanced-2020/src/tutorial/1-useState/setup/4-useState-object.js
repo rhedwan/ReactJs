c9cd610ee7bf556ca7ffc957c15e2171a843b608
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 11:01:40 +0100

    useState with an object

--- react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Peter",
+    age: 24,
+    message: "random message",
+  });
+  // console.log(person);
+  const changeMesage = () => {
+    setPerson({...person, message : 'new message' });
+  }
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMesage}>
+        changeMesage
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
