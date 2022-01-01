6f97b85a577a848a8c450da1b4fe2cc22b6bf534
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 09:58:13 +0100

    Structured the folder and render Component 😀😊

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Components
+import {Counter} from "./topics/useState-Counter"
+
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Counter/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ docs/src/topics/useState-Counter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Counter(){
+    return (
+        <React.Fragment>
+            <h1>Building a Counter with useState</h1>
+        </React.Fragment>
+    )
+}
