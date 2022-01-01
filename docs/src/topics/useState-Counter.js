6f65f6ef33343d6fcbd623808e9d1910251d282d
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 10:10:34 +0100

    Made a multiplier counter

--- docs/src/topics/useState-Counter.js
+++ docs/src/topics/useState-Counter.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 
 export function Counter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(2);
   return (
     <React.Fragment>
       <h1>Building a Counter with useState</h1>
       <div>
         <h3>You have clicked the button {count} times 😉</h3>
-        <button onClick={
-            () =>{
-                setCount(count + 1)
-            }
-        }>Click Me</button>
+        <button
+          onClick={() => {
+            setCount(count * count);
+          }}
+        >
+          Click Me
+        </button>
       </div>
     </React.Fragment>
   );
