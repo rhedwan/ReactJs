31a3787a3e919432072e7b4701eb736dc2d5c915
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 11:40:58 +0100

    Used an handler function in reset

--- react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0) ;
+  const reset = () => setValue(0);
   return (
     <React.Fragment>
       <section style={
@@ -13,7 +14,7 @@ const UseStateCounter = () => {
       <h1>{value}</h1>
       <button className="btn" onClick={() => setValue(value - 1)}>Decrease</button>
       <button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
-      <button className="btn" onClick={() => setValue(0)}>Reset</button>
+      <button className="btn" onClick={reset}>Reset</button>
       </section>
     </React.Fragment>
   );
