4bcc637e60441be20a61d820007fc303b6f557e9
rhedwan <adesolaridwan2003@gmail.com>
2022-01-01 10:26:28 +0100

    Added some CSS styling to component

--- docs/src/topics/useState-Counter.js
+++ docs/src/topics/useState-Counter.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 
 export function Counter() {
-  const [count, setCount] = useState(2);
+  const [count, setCount] = useState(1);
   return (
     <React.Fragment>
       <h1>Building a Counter with useState</h1>
-      <div>
+      <div className="counter">
         <h3>You have clicked the button {count} times 😉</h3>
         <button
           onClick={() => {
-            setCount(count * count);
+            setCount(count + 1);
           }}
         >
           Click Me
