91186b2e3f52dcbb36d72bba096d279ee2d171c9
rhedwan <adesolaridwan2003@gmail.com>
2022-01-02 14:52:56 +0100

    useEffect: missing dependency make it run on inital render only

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -10,7 +10,7 @@ const UseEffectBasics = () => {
     if(value > 1){
       document.title = `New Messages(${value})`
     }
-  })
+  }, [])
   console.log('Render Component')
   return <React.Fragment>
     <h1>{value}</h1>
