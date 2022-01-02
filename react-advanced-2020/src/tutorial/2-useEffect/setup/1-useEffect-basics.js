452faae17809c53535ba2b9243769ae645e4998e
rhedwan <adesolaridwan2003@gmail.com>
2022-01-02 14:56:19 +0100

    useEffect: passing of dependency in the array

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   useEffect(() => {
-
-    console.log('Call Use effect')
-    if(value > 1){
-      document.title = `New Messages(${value})`
+    console.log("Call Use effect");
+    if (value >= 1) {
+      document.title = `New Messages(${value})`;
     }
-  }, [])
-  console.log('Render Component')
-  return <React.Fragment>
-    <h1>{value}</h1>
-    <button className="btn" onClick={() => setValue(value +1)}>Click Me</button>
-  </React.Fragment>;
+  }, [value]);
+  console.log("Render Component");
+  return (
+    <React.Fragment>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectBasics;
