c9cf55e5afcbf4c3482ad72b6c0c991f8c0c12c7
rhedwan <adesolaridwan2003@gmail.com>
2022-01-02 14:30:15 +0100

    useEffect Basics

--- react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,12 +3,16 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
+  const [value, setValue] = useState(0);
   useEffect(() => {
     console.log('Call Use effect')
-    
+    document.title = `New Messages(${value})`
   })
   console.log('Render Component')
-  return <h2>useEffect Basics</h2>;
+  return <React.Fragment>
+    <h1>{value}</h1>
+    <button className="btn" onClick={() => setValue(value +1)}>Click Me</button>
+  </React.Fragment>;
 };
 
 export default UseEffectBasics;
