5a843b2f0ff001c10f9ff72c5132ce33870f6298
rhedwan <adesolaridwan2003@gmail.com>
2022-01-02 17:09:48 +0100

    useEffect: fetching of data 'https://api.github.com/users'

--- react-advanced-2020/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+  const getUsers = async function () {
+    const response = await fetch(url);
+    const users = await response.json();
+    setUsers(users);
+    // console.log(users);
+  };
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return (
+    <React.Fragment>
+      <h3>Github Users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectFetchData;
