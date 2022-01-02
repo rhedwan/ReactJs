8a5d8e6e0ebe972307b276707a8329d73eaf0bb5
rhedwan <adesolaridwan2003@gmail.com>
2022-01-02 15:16:25 +0100

    useEffect: Fetching the windows width on resizing

--- react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+
+  });
+  console.log(size);
+  return (
+    <React.Fragment>
+      <h2>Window</h2>
+      <h2>{size} PX</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
