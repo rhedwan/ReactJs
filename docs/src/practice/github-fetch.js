e6fa201fde0d07b71c39dfdda84e76f280ed335c
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 17:49:23 +0100

    Worked on the 'onChange' event for the input

--- docs/src/practice/github-fetch.js
+++ docs/src/practice/github-fetch.js
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
 
 function UserDisplay() {
+  const [user, setUser] = useState("");
+  //   https://api.github.com/users/
+  const fetchUser = () => {};
+  
+  const updateValue = (e) => {
+    setUser(e.target.value);
+  };
   return (
     <div className="container">
       <h2>Github User</h2>
       <article>
-        <form className="form">
+        <form className="form" onSubmit={fetchUser}>
           <div className="form-control">
             <label htmlFor="userName">Username</label>
-            <input type="text" id="userName" name="userName" />
+            <input
+              type="text"
+              id="userName"
+              name="userName"
+              value={user}
+              onChange={updateValue}
+            />
           </div>
           <button type="submit">Click</button>
         </form>
