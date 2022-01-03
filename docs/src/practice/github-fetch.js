88060d718e568f3cb32dd4d0e7312049ecb49e19
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 17:39:20 +0100

    Component UI implementation

--- docs/src/practice/github-fetch.js
+++ docs/src/practice/github-fetch.js
@@ -4,6 +4,15 @@ function UserDisplay() {
   return (
     <div className="container">
       <h2>Github User</h2>
+      <article>
+        <form className="form">
+          <div className="form-control">
+            <label htmlFor="userName">Username</label>
+            <input type="text" id="userName" name="userName" />
+          </div>
+          <button type="submit">Click</button>
+        </form>
+      </article>
     </div>
   );
 }
