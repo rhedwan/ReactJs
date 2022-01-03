af27f768ec8c9973fcc52aa110e03d6541d0d49b
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 18:37:58 +0100

    Finally implemented the main react component

--- docs/src/practice/github-fetch.js
+++ docs/src/practice/github-fetch.js
@@ -2,12 +2,36 @@ import React, { useState } from "react";
 
 function UserDisplay() {
   const [user, setUser] = useState("");
-  //   https://api.github.com/users/
-  const fetchUser = () => {};
-  
+  const [displayUser, setDisplayUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchUser = (e) => {
+    e.preventDefault();
+    if (!user) return;
+    fetch(`https://api.github.com/users/${user}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setDisplayUser(data);
+        setIsLoading(!isLoading);
+      });
+  };
+
   const updateValue = (e) => {
     setUser(e.target.value);
   };
+
+  const Loading = () => {
+    return isLoading ? (
+      <React.Fragment>
+        <img src={displayUser.avatar_url} alt="" />
+        <h2>{displayUser.name}</h2>
+      </React.Fragment>
+    ) : (
+      <h1>Nothing to Fectch</h1>
+    );
+  };
+
   return (
     <div className="container">
       <h2>Github User</h2>
@@ -26,6 +50,7 @@ function UserDisplay() {
           <button type="submit">Click</button>
         </form>
       </article>
+      <Loading />
     </div>
   );
 }
