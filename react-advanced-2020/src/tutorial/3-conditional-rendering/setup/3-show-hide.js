4dbc0e95d4d654fe55bd1917bf86f23fa2a7b781
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 09:46:40 +0100

    useState & useEffect:Cleanup of the useEffect function after eventlistening

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -13,6 +13,17 @@ const ShowHide = () => {
 };
 
 const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
   return (
     <div
       style={{
@@ -20,7 +31,7 @@ const Item = () => {
       }}
     >
       <h1>Window</h1>
-      <h2>Size: </h2>
+      <h2>Size: {size}px</h2>
     </div>
   );
 };
