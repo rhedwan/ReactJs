a3bbd80b8d95771d749169ed8cb151eeeecf76cc
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 09:24:12 +0100

    useEffect:Conditional rendering with ternary operator

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -27,6 +27,13 @@ const ShortCircuit = () => {
         Toggle Error
       </button>
       {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>There is an error...</p>
+      ) : (
+        <div>
+          <h2>There is no error</h2>
+        </div>
+      )}
     </React.Fragment>
   );
 };
