357fab90f94936874b893132fce93767ce7380a3
rhedwan <adesolaridwan2003@gmail.com>
2022-01-03 09:13:36 +0100

    useEffect: Made a toggleable btn with ternary operator

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,16 +4,28 @@ import React, { useState } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
-  const firstValue = text || "hello world";
-  const secondValue = text && "hello world";
+  const [isError, setError] = useState(false);
+
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
   return (
     <React.Fragment>
       {/* <h1>{firstValue}</h1>
-      <h1>Value: {secondValue}</h1>  */}
-      <h1>{text || "Rhedwan"}</h1>
+      <h1>Value: {secondValue}</h1>  
       {!text && <h1>Hello World</h1> }
-      {text && <h1>Hello World 😊</h1> }
+    {text && <h1>Hello World 😊</h1> } */}
+
+      <h1>{text || "Rhedwan"}</h1>
+      <button
+        className="btn"
+        onClick={() => {
+          return isError ? setError(false) : setError(true);
+        }}
+      >
+        Toggle Error
+      </button>
+      {isError && <h1>Error...</h1>}
     </React.Fragment>
   );
 };
