c18c334f187f86cc3a25f3915cf29ecf0df3bcd6
rhedwan <adesolaridwan2003@gmail.com>
2022-01-04 07:10:00 +0100

    Worked on useRef(preserving) & useEffect(for re-render)

--- react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ react-advanced-2020/src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -6,10 +6,19 @@ import React, { useEffect, useRef } from "react";
 
 const UseRefBasics = () => {
   const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(refContainer.current.value);
+    console.log(divContainer.current);
   };
+
+  useEffect(() => {
+    console.log(refContainer.current);
+    refContainer.current.focus();
+  });
+  
   console.log(refContainer);
   return (
     <form className="form" onSubmit={handleSubmit}>
@@ -17,6 +26,7 @@ const UseRefBasics = () => {
         <input type="text" ref={refContainer} />
         <button type="submit">Submit</button>
       </div>
+      <div ref={divContainer}>Hello World</div>
     </form>
   );
 };
