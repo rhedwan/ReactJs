3b827a628ebe54b8757332dcf46ca4ccc075d72a
rhedwan <adesolaridwan2003@gmail.com>
2022-01-04 07:40:56 +0100

    Adding of new item to a list

--- react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,43 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModal, setShowModal] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) return setShowModal(true);
+    setShowModal(true);
+    setPeople([...people, { id: new Date().getTime().toString(), name }]);
+    setName("");
+  };
+
+  return (
+    <React.Fragment>
+      {showModal && <Modal />}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default Index;
