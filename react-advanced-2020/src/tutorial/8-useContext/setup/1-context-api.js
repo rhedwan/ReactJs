4c2c59a8d86ec6eac892d7b5252826a9f0b85aca
rhedwan <adesolaridwan2003@gmail.com>
2022-01-04 19:31:49 +0100

    useContext: Accessing of context from a parent component

--- react-advanced-2020/src/tutorial/8-useContext/setup/1-context-api.js
+++ react-advanced-2020/src/tutorial/8-useContext/setup/1-context-api.js
@@ -1,19 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
-const PropDrilling = () => {
+const PersonCentext = React.createContext();
+// two components - Provider, Consumer
+
+const ContextAPI = () => {
   const removePerson = (id) => {
     setPeople((people) => people.filter((person) => person.id !== id));
   };
   const [people, setPeople] = useState(data);
   return (
-    <section>
+    <PersonCentext.Provider value="hello">
       <h3>prop drilling</h3>
       <List people={people} removePerson={removePerson} />
-    </section>
+    </PersonCentext.Provider>
   );
 };
 
@@ -34,6 +37,8 @@ const List = ({ people, removePerson }) => {
 };
 
 const SingePerson = ({ id, name, removePerson }) => {
+  const data = useContext(PersonCentext);
+  console.log(data);
   return (
     <div className="item">
       <h4>{name}</h4>
@@ -41,4 +46,4 @@ const SingePerson = ({ id, name, removePerson }) => {
     </div>
   );
 };
-export default PropDrilling;
+export default ContextAPI;
