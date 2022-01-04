b5c30c361020ba4c25e4d1126a04af177550db6b
rhedwan <adesolaridwan2003@gmail.com>
2022-01-04 18:53:14 +0100

    Prop drilling: Worked on the remove button

--- react-advanced-2020/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ react-advanced-2020/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -5,29 +5,39 @@ import { data } from "../../../data";
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
+  const removePerson = (id) => {
+    setPeople((people) => people.filter((person) => person.id !== id));
+  };
   const [people, setPeople] = useState(data);
   return (
     <section>
       <h3>prop drilling</h3>
-      <List people={people} />
+      <List people={people} removePerson={removePerson} />
     </section>
   );
 };
 
-const List = ({ people }) => {
+const List = ({ people, removePerson }) => {
   return (
     <React.Fragment>
       {people.map((person) => {
-        return <SingePerson key={person.id} person={person} />;
+        return (
+          <SingePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        );
       })}
     </React.Fragment>
   );
 };
 
-const SingePerson = ({ id, name }) => {
+const SingePerson = ({ id, name, removePerson }) => {
   return (
     <div className="item">
-      <h4>Single Item</h4>
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>Remove</button>
     </div>
   );
 };
