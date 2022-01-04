0359f9e4b00a93168e36807bfcd7bc02e29285ba
rhedwan <adesolaridwan2003@gmail.com>
2022-01-04 18:42:55 +0100

    Prop drilling: render nested components 😀

--- react-advanced-2020/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ react-advanced-2020/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <section>
+      <h3>prop drilling</h3>
+      <List people={people} />
+    </section>
+  );
 };
 
+const List = ({ people }) => {
+  return (
+    <React.Fragment>
+      {people.map((person) => {
+        return <SingePerson key={person.id} person={person} />;
+      })}
+    </React.Fragment>
+  );
+};
+
+const SingePerson = ({ id, name }) => {
+  return (
+    <div className="item">
+      <h4>Single Item</h4>
+    </div>
+  );
+};
 export default PropDrilling;
