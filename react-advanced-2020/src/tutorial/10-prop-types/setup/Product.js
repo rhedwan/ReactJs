6221e33519650138b1be52901d9d7f605709ed34
rhedwan <adesolaridwan2003@gmail.com>
2022-01-05 17:59:16 +0100

    Bug:Raising of errors using PropTypes

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Product = ({ image, name, price }) => {
+  console.log(image, name, price);
   return (
     <article className="product">
-      <img src={image.url} alt={name} />
+      {/* <img src={image.url} alt={name} />
       <h4>{name}</h4>
-      <p>${price}</p>
+      <p>${price}</p> */}
     </article>
   );
 };
 
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
 export default Product;
