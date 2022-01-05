19d94555e5eccaf4be297cd85e1697cba4cbf000
rhedwan <adesolaridwan2003@gmail.com>
2022-01-05 18:07:57 +0100

    Fixed the 'price' issue

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
 const Product = ({ image, name, price }) => {
   console.log(image, name, price);
   return (
     <article className="product">
-      {/* <img src={image.url} alt={name} />
+      <img src={image.url} alt={name} />
       <h4>{name}</h4>
-      <p>${price}</p> */}
+      <p>${price}</p>
     </article>
   );
 };
@@ -16,5 +17,11 @@ Product.propTypes = {
   image: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+};
+
+Product.defaultProps = {
+  name: "Default name",
+  price: 3.99,
+  image: defaultImage,
 };
 export default Product;
