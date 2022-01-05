08916ed5726848606d61729af37af5053a7ba498
rhedwan <adesolaridwan2003@gmail.com>
2022-01-05 19:43:29 +0100

    React-router: fetching of individual items

--- react-advanced-2020/src/tutorial/11-react-router/setup/Person.js
+++ react-advanced-2020/src/tutorial/11-react-router/setup/Person.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { data } from "../../../data";
 import { Link, useParams } from "react-router-dom";
 const Person = () => {
-  const { name, setName } = useState("default name");
+  const [name, setName] = useState("default name");
   const { id } = useParams();
-
-  console.log(useParams());
+  useEffect(() => {
+    const newPerson = data.find((person) => person.id === +id);
+    setName(newPerson.name);
+  }, []);
   return (
     <div>
       <h2>{name}</h2>
