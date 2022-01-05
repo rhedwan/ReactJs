f4f4fa13eb889f06fb229298fe08eb71152fb609
rhedwan <adesolaridwan2003@gmail.com>
2022-01-05 20:42:47 +0100

    'addToCart' function bug for re-rendering

--- react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -10,32 +10,43 @@ const url = "https://course-api.com/javascript-store-products";
 const Index = () => {
   const { products } = useFetch(url);
   const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
 
+  const addToCart = () => {
+    setCart(cart + 1);
+  };
   return (
     <React.Fragment>
       <h1>Count : {count}</h1>
       <button className="btn" onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: "3rem" }}>Cart :{cart}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </React.Fragment>
   );
 };
 
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   useEffect(() => {
     console.log("big list call");
   });
   return (
     <section className="products">
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>;
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        );
       })}
     </section>
   );
 });
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count("big list call");
   });
@@ -48,6 +59,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>Add to cart</button>
     </article>
   );
 };
