11a5e0eff7af5c10ae7abcad847f75242824de50
rhedwan <adesolaridwan2003@gmail.com>
2022-01-08 18:12:41 +0100

    Made the fetch & conditional rendering

--- react-projects/02-tours/setup/src/App.js
+++ react-projects/02-tours/setup/src/App.js
@@ -1,11 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+  const fetchTours = async () => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  fetchTours()
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
 }
 
-export default App
+export default App;
