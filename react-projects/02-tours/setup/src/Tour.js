99e5cc8c302695537543ba819cbbe696371d0987
rhedwan <adesolaridwan2003@gmail.com>
2022-01-08 22:01:50 +0100

    Worked on the toggle action:read more/less

--- react-projects/02-tours/setup/src/Tour.js
+++ react-projects/02-tours/setup/src/Tour.js
@@ -1,5 +1,6 @@
 import React from "react";
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = React.useState(false);
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -8,7 +9,12 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">${price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
         <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
