680a72961d0fb2568e14c057166165d71f6c1117
rhedwan <adesolaridwan2003@gmail.com>
2022-01-09 14:11:52 +0100

    Accordion:Toggleable action of the content

--- react-projects/04-accordion/setup/src/App.js
+++ react-projects/04-accordion/setup/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and anwser about login</h3>
+        <sectoin className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </sectoin>
+      </div>
+    </main>
+  );
 }
 
 export default App;
