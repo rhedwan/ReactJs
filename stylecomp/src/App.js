662e91f788051dbdf33eb4ce4fce1dfc4a7ab2de
rhedwan <adesolaridwan2003@gmail.com>
2022-01-15 08:39:38 +0100

    Using of styled components

--- stylecomp/src/App.js
+++ stylecomp/src/App.js
@@ -1,9 +1,10 @@
+import { Container } from "./components/styles/Container.styled";
 
 function App() {
   return (
-    <div className="container">
+    <Container>
       <h1>Hello World</h1>
-    </div>
+    </Container>
   );
 }
 
