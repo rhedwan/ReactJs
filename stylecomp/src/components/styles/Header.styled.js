d9b30be3eeee6ce8b147de2f70c750c06885a175
rhedwan <adesolaridwan2003@gmail.com>
2022-01-16 23:49:41 +0100

    Using of ThemeProvider

--- stylecomp/src/components/styles/Header.styled.js
+++ stylecomp/src/components/styles/Header.styled.js
@@ -1,13 +1,7 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  background-color: ${(props) => props.bg};
+  background-color: ${({ bg }) => bg};
   padding: 40px 0;
 
-  /* h1 {
-    color: red;
-  }
-  &:hover {
-    background-color: black;
-  } */
 `;
