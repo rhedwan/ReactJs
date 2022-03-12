0e9e76350f2b27debc83dd515160f420cdec8f6a
rhedwan <adesolaridwan2003@gmail.com>
2022-03-12 18:30:18 +0100

    Variables accessing in JSX

--- react-advanced-2020/src/index.js
+++ react-advanced-2020/src/index.js
@@ -8,31 +8,25 @@ function BookList() {
   return (
     <section className="bookList">
       <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
+const author = "Amelia Hepworth";
 const Book = () => {
+  const title = "I Love You to the Moon and Back";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
+        alt=""
+      />
+      <h1>{title} </h1>
+      <h4> {author.toUpperCase()}</h4>
+      <p>{/* let x= 1 */}</p>
+      <p>{6+5}</p>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back </h1>;
-const Author = () => <h4> Amelia Hepworth</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
