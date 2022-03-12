757c716a4adf8211d22860cdfda7e0baa192f4cd
rhedwan <adesolaridwan2003@gmail.com>
2022-03-12 18:46:41 +0100

    Using props

--- react-advanced-2020/src/index.js
+++ react-advanced-2020/src/index.js
@@ -4,27 +4,30 @@ import ReactDom from "react-dom";
 // CSS
 import "./indexStyle.css";
 
+// SetUp
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg";
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
+      <Book job="developer" />
+      <Book title="Random title" number={33} />
     </section>
   );
 }
 
-const author = "Amelia Hepworth";
-const Book = () => {
-  const title = "I Love You to the Moon and Back";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg"
-        alt=""
-      />
+      <img src={img} alt="" />
       <h1>{title} </h1>
-      <h4> {author.toUpperCase()}</h4>
-      <p>{/* let x= 1 */}</p>
-      <p>{6+5}</p>
+      <h4> {author}</h4>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
