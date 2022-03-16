c900f5c71760e1179e9c272d5544fdf7016fbeec
rhedwan <adesolaridwan2003@gmail.com>
2022-03-16 22:18:56 +0100

    Events handlers in JSX

--- react-advanced-2020/src/index.js
+++ react-advanced-2020/src/index.js
@@ -38,11 +38,23 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const clickHandler = () => {
+    alert("Hello World");
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title} </h1>
+      <h1 onClick={() => console.log("It the h1 element")}>{title} </h1>
       <h4> {author}</h4>
+      <button type="buuton" onClick={clickHandler}>
+        Refrence Example
+      </button>
+      <button type="button" onClick={() =>complexExample(author)}>
+        More Complex Example
+      </button>
     </article>
   );
 };
