464d776706f963e9f0b920c7647df3c3e9cd6651
rhedwan <adesolaridwan2003@gmail.com>
2022-03-16 22:23:32 +0100

    Events handlers in JSX with refrences

--- react-advanced-2020/src/index.js
+++ react-advanced-2020/src/index.js
@@ -38,21 +38,25 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
-  const clickHandler = () => {
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
     alert("Hello World");
   };
   const complexExample = (author) => {
     console.log(author);
   };
   return (
-    <article className="book">
+    <article className="book" onMouseOver={() => {
+      console.log(title)
+    }}>
       <img src={img} alt="" />
       <h1 onClick={() => console.log("It the h1 element")}>{title} </h1>
       <h4> {author}</h4>
       <button type="buuton" onClick={clickHandler}>
         Refrence Example
       </button>
-      <button type="button" onClick={() =>complexExample(author)}>
+      <button type="button" onClick={() => complexExample(author)}>
         More Complex Example
       </button>
     </article>
