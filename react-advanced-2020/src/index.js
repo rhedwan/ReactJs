4270359553876afc3cab2ba44fec7e533337c77e
rhedwan <adesolaridwan2003@gmail.com>
2022-03-16 16:17:41 +0100

    list of items

--- react-advanced-2020/src/index.js
+++ react-advanced-2020/src/index.js
@@ -5,37 +5,25 @@ import ReactDom from "react-dom";
 import "./indexStyle.css";
 
 // SetUp
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
-  title: "Our Class is a family",
-  author: "Shannon Olsen",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a family",
+    author: "Shannon Olsen",
+  },
+];
 
+const names = ["Ridwan", "Pidoxy", "Bayo"];
+const newName = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 function BookList() {
-  return (
-    <section className="bookList">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing Lorem ipsum dolor
-          sit amet consectetur adipisicing.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="bookList">{newName}</section>;
 }
 
 const Book = (props) => {
@@ -46,7 +34,6 @@ const Book = (props) => {
       <img src={img} alt="" />
       <h1>{title} </h1>
       <h4> {author}</h4>
-      {props.children}
     </article>
   );
 };
