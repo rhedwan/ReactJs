8469056f15abe98fd66dcce90cc4bd2c3f145e7b
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-21 10:45:31 +0100

    covered useState basics

--- 2022/react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
+++ 2022/react-advanced-2020/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("randon title");
+  const handleClick = () => {
+    if (text === "randon title") setText("Hello World");
+    else setText("randon title");
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- 2022/react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
+++ 2022/react-advanced-2020/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Bola",
+    age: 45,
+    message: "Random message",
+  });
+  const changeMessage = () => {
+    setPerson({ ...person, message: "helo world" });
+  };
+  return (
+    <React.Fragment>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateObject;
