f0bc09eb6a69f2f2c683bc18dff345b1fec55641
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-23 16:08:41 +0100

    finish project to 07

--- 2022/react-projects/02-tours/setup/src/App.js
+++ 2022/react-projects/02-tours/setup/src/App.js
@@ -1,11 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    return setTours(tours.filter((tour) => tour.id !== id));
+  };
+  const fetchTour = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTour();
+  }, []);
+  if (loading)
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>NO tours left</h2>
+          <button className="btn" onClick={fetchTour}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
