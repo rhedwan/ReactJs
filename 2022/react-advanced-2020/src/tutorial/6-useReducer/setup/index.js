b0e598532a215301cb9e45bf6d0376790b186784
Adeyemo Ridwan <adesolaridwan2003@gmail.com>
2022-06-27 14:17:04 +0100

    createContext and useContext

--- 2022/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ 2022/react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,65 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
+import { reducer } from "./reducer";
 // reducer function
 
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: "",
+};
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState("");
+  // const [people, setPeople] = useState(data);
+  // const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({ type: "ADD_PERSON", payload: newItem });
+      setName("");
+    } else {
+      dispatch({ type: "NO_VALUE" });
+    }
+  };
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
+  };
+  return (
+    <React.Fragment>
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn">
+          add
+        </button>
+      </form>
+      {state.people.map((person) => {
+        return (
+          <div key={person.id} className="item">
+            <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: "REMOVE_ITEM", payload: person.id })
+              }
+            >
+              remove
+            </button>
+          </div>
+        );
+      })}
+    </React.Fragment>
+  );
 };
 
 export default Index;
--- 2022/react-projects/12-sidebar-modal/setup/src/Sidebar.js
+++ 2022/react-projects/12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,46 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  const { isSideBarOpen, closeSidebar } = useGlobalContext();
 
-export default Sidebar
+  return (
+    <div className={`${isSideBarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="coding addict" />
+        <div className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </div>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, icon, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          const { id, url, icon, text } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon} {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
